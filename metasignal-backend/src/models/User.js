77e0be1e68ce05293a2bc88028afdd9201859dcb
Dhruv Pancholi <160162577+dhruv457457@users.noreply.github.com>
2025-06-22 22:59:22 +0530

    old remove backend

--- metasignal-backend/src/models/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  address: { type: String, unique: true, required: true },
-  follows: { type: [String], default: [] },
-});
-
-export default mongoose.model("User", userSchema);
